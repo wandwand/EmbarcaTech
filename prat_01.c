0035c2cd9eb3eec56f468b0ad42c5f331ca84144
Ian Wanderley Gomes <wanderleygomesian@gmail.com>
2024-11-25 08:40:39 -0300

    Adicionada function raizes do polinomio

--- prat_01.c
+++ prat_01.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
-   double a, b, c, delta;
-   double raiz_01, raiz_02;
+void raiz(double a, double b, double c); // Declaração da função
 
-   printf("Digite os coeficientes da eq de 2 grau (a, b, c): ");
-   scanf("%lf %lf %lf", &a, &b, &c); // Corrigido: adicionados os '&' nos argumentos
+int main() {
+    double a, b, c;
 
-   delta = pow(b, 2) - 4 * a * c;
-   raiz_01 = -b + pow(delta,0.5);
-   raiz_02 = -b - pow(delta,0.5);
-   printf("O valor de delta eh: %.2lf\n", delta);
-   printf("O valor das raizes eh: %.2lf\n e %.2lf\n", raiz_01,raiz_02);
-   return 0;
+    // Coeficientes da equação
+    a = 1;
+    b = 0;
+    c = 1;
+
+    raiz(a, b, c); // Chamada da função
+    return 0;
+}
+
+void raiz(double a, double b, double c) { // Definição da função
+    double delta, raiz_01, raiz_02;
+
+    delta = pow(b, 2) - 4 * a * c;
+
+    if (delta >= 0) { // Tratamento para raízes reais
+        raiz_01 = (-b + sqrt(delta)) / (2 * a);
+        raiz_02 = (-b - sqrt(delta)) / (2 * a);
+        printf("O valor de delta é: %.2lf\n", delta);
+        printf("As raízes reais são: %.2lf e %.2lf\n", raiz_01, raiz_02);
+    } else { // Tratamento para raízes complexas
+        double parte_real = -b / (2 * a);
+        double parte_imaginaria = sqrt(-delta) / (2 * a);
+        printf("O valor de delta é: %.2lf\n", delta);
+        printf("As raízes complexas são: %.2lf + %.2lfi e %.2lf - %.2lfi\n", 
+               parte_real, parte_imaginaria, parte_real, parte_imaginaria);
+    }
 }
